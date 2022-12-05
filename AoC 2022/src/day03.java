89f1f74d9dce4809fb3a8c939e5c56553618e545
Florin Buffet <Florin Buffet@10.5.23.200>
2022-12-05 15:36:56 +0100

    AoC 2022 Day 3 Formatting

--- AoC 2022/src/day03.java
+++ AoC 2022/src/day03.java
@@ -10,7 +10,7 @@ public class day03 {
 	public static void main(String[] args) throws FileNotFoundException {
 		File file = new File("data/day03.txt");
 		Scanner scan = new Scanner(file);
-		
+
 		String alphabet = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		int[] packedPart1 = new int[53];
 		int[] packedPart2 = new int[53];
@@ -21,35 +21,33 @@ public class day03 {
 		while (scan.hasNextLine()) {
 			String line = scan.nextLine();
 			elfGroup++;
-			for (int i = 0; i<line.length()/2; i++) {
+			for (int i = 0; i < line.length() / 2; i++) {
 				char ch = line.charAt(i);
-				packedPart1[alphabet.indexOf(ch)] ++;
+				packedPart1[alphabet.indexOf(ch)]++;
 			}
-			for (int i = line.length()/2; i<line.length(); i++) {
+			for (int i = line.length() / 2; i < line.length(); i++) {
 				char ch = line.charAt(i);
-				if (packedPart1[alphabet.indexOf(ch)]>0) {
+				if (packedPart1[alphabet.indexOf(ch)] > 0) {
 					part1 += alphabet.indexOf(ch);
 					packedPart1[alphabet.indexOf(ch)] = 0;
 				}
 			}
-			
-			
-			for (int i = 0; i<line.length(); i++) {
+
+			for (int i = 0; i < line.length(); i++) {
 				char ch = line.charAt(i);
 				if (elfGroup == 1) {
 					packedPart2[alphabet.indexOf(ch)] = 1;
 				} else if ((elfGroup == 2) && (packedPart2[alphabet.indexOf(ch)] == 1)) {
 					packedPart2[alphabet.indexOf(ch)] = 2;
 				} else if (elfGroup == 3) {
-					if (packedPart2[alphabet.indexOf(ch)]> 1) {
+					if (packedPart2[alphabet.indexOf(ch)] > 1) {
 						part2 += alphabet.indexOf(ch);
 						packedPart2[alphabet.indexOf(ch)] = 0;
 					}
 				}
 			}
-			
-			
-			for (int i = 0; i<53; i++) {
+
+			for (int i = 0; i < 53; i++) {
 				packedPart1[i] = 0;
 				if (elfGroup == 3) {
 					packedPart2[i] = 0;
@@ -58,8 +56,8 @@ public class day03 {
 			elfGroup = elfGroup % 3;
 		}
 		scan.close();
-		System.out.println("Teil 1: "+part1);
-		System.out.println("Teil 2: "+part2);
+		System.out.println("Teil 1: " + part1);
+		System.out.println("Teil 2: " + part2);
 	}
 
 }
