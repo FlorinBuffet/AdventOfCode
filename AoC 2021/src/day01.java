c91d1cd7ce3abaffee55a41057e33efd1eac32cd
Florin Buffet <fbuffet@student.ethz.ch>
2022-12-06 00:16:33 +0100

    AoC 2021 Day 1 solved

--- AoC 2021/src/day01.java
+++ AoC 2021/src/day01.java
@@ -11,12 +11,28 @@ public class day01 {
 		File file = new File("data/day01.txt");
 		Scanner scan = new Scanner(file);
 
+		int lastNum = Integer.MAX_VALUE;
+		int part1 = 0;
+		int part2 = -3; // as the first three always have the increment
+		int[] lastvalues = new int[3];
 
 		while (scan.hasNextLine()) {
-			String line = scan.nextLine();
-			
+			int number = scan.nextInt();
+			if (number > lastvalues[2])
+				part1++;
+
+			int sumOfLastTwo = lastvalues[1] + lastvalues[2];
+			if (sumOfLastTwo + number > sumOfLastTwo + lastvalues[0])
+				part2++;
+
+			lastvalues[0] = lastvalues[1];
+			lastvalues[1] = lastvalues[2];
+			lastvalues[2] = number;
 		}
 		scan.close();
+
+		System.out.println("Teillösung 1: " + part1);
+		System.out.println("Teillösung 2: " + part2);
 	}
 
 }
